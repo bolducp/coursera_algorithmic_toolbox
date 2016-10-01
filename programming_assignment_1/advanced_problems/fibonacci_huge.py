3bc8d50bd4910864f9c73f744f2b5a9c591982b7
Paige Bolduc <paigebolduc@gmail.com>
2016-10-01 19:44:50 -0400

    Refactor solution to use pisano period more effectively.

--- programming_assignment_1/advanced_problems/fibonacci_huge.py
+++ programming_assignment_1/advanced_problems/fibonacci_huge.py
@@ -1,38 +1,41 @@
+# Uses python2
 import sys
 
 def get_fibonacci_huge(n, m):
     pisano_period_length = get_pisano_period_length(m)
     pisano_period_remainder = n % pisano_period_length
-    return get_fibonacci(pisano_period_remainder) % m
-   
+
+    last = 0
+    second_last = 1
+
+    fib = pisano_period_remainder
+
+    for num in xrange(1, pisano_period_remainder):
+        fib = (last + second_last) % m
+        last = second_last
+        second_last = fib
+
+    return fib % m
+
+
 def get_pisano_period_length(m):
     if m == 0:
         return 0
-
-    fibs = [0, 1, 1]
-    pisano_period = [0, 1]
-    pisano_period.append(1 % m)
-
-    while not (pisano_period[-2] == 0 and pisano_period[-1] == 1):
-        next_fib = fibs[-1] + fibs[-2]
-        fibs.append(next_fib)
-        next_pisano = next_fib % m
-        pisano_period.append(next_pisano)
-
-    return len(pisano_period) - 2
-
-def get_fibonacci(n):
-    if n == 0:
-        return 0
-
-    fibs = [0, 1]
-    while len(fibs) <= n:
-        next_fib = fibs[-1] + fibs[-2]
-        fibs.append(next_fib)
-    return fibs[-1]
+    pisano_period_second_last = 0
+    pisano_period_last = 1
     
+    for num in xrange(0, m * m):
+        next_pisano = (pisano_period_second_last + pisano_period_last) % m
+        pisano_period_second_last = pisano_period_last
+        pisano_period_last = next_pisano
+        if (pisano_period_second_last == 0 and pisano_period_last == 1):
+            return num + 1
+
 
 if __name__ == '__main__':
     input = sys.stdin.read();
     n, m = map(int, input.split())
     print(get_fibonacci_huge(n, m))
+
+
+# print(get_fibonacci_huge(2816213588, 30524))
